607447796f4cb3527bc808d1c6869e9b15831ce1
nvb-uy <elocindev@lunapixel.gg>
2024-06-20 11:38:35 -0300

    feat(new colors for weather events)

--- shaders/include/light/colors/weather_color.glsl
+++ shaders/include/light/colors/weather_color.glsl
@@ -6,15 +6,15 @@
 uniform float biome_may_sandstorm;
 
 vec3 get_rain_color() {
-	return mix(0.033, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.49, 0.65, 1.00);
+	return mix(0.033, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.55, 0.55, 0.60);
 }
 
 vec3 get_snow_color() {
-	return mix(0.060, 1.60, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.49, 0.65, 1.00);
+	return mix(0.060, 1.60, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.55, 0.55, 0.65);
 }
 
 vec3 get_sandstorm_color() {
-	return mix(0.033, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(1.00, 0.83, 0.60);
+	return mix(0.033, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.80, 0.73, 0.50);
 }
 
 vec3 get_weather_color() {
